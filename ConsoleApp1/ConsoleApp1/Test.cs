3d265bbd3b257c235cb067c4408fee771744f90b
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2025-11-23 15:42:39 +0200

    Add logic to Tests

--- ConsoleApp1/ConsoleApp1/Test.cs
+++ ConsoleApp1/ConsoleApp1/Test.cs
@@ -1,6 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 namespace lab_Interfaces
 {
+    public class Program
+    {
+        static void TestAPlusBSquare<T>(T a, T b) where T : IMyNumber<T>
+        {
+            Console.WriteLine("=== (a+b)^2 test, a = " + a + ", b = " + b + " ===");
 
+            T aPlusB = a.Add(b);
+            Console.WriteLine("a + b = " + aPlusB);
+
+            T left = aPlusB.Multiply(aPlusB);
+            Console.WriteLine("(a + b)^2 = " + left);
+
+            T right = a.Multiply(a); 
+            T ab = a.Multiply(b);  
+            T twoab = ab.Add(ab);  
+            T b2 = b.Multiply(b);  
+
+            right = right.Add(twoab);
+            right = right.Add(b2);
+
+            Console.WriteLine("a^2 + 2ab + b^2 = " + right);
+            Console.WriteLine();
+        }
+    }
 }
