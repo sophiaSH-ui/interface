a903b266b2156a850b62304fed58cc29d63fea4f
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2025-12-10 21:44:55 +0200

    Fix tests

--- ConsoleApp1/ConsoleApp1/Test.cs
+++ ConsoleApp1/ConsoleApp1/Test.cs
@@ -27,5 +27,46 @@ namespace lab_Interfaces
             Console.WriteLine("=== Finishing testing (a+b)^2=a^2+2ab+b^2 with a = " + a + ", b = " + b + " ===");
             Console.WriteLine();
         }
+
+        public static void TestSquaresDifference<T>(T a, T b) where T : IMyNumber<T>
+        {
+            Console.WriteLine("=== Starting testing (a-b) = (a^2-b^2)/(a+b) with a = " + a + ", b = " + b + " ===");
+
+            T aMinusB = a.Subtract(b);
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("(a - b) = " + aMinusB);
+
+            Console.WriteLine(" = = = ");
+
+            // a^2
+            T a2 = a.Multiply(a);
+            Console.WriteLine("a^2 = " + a2);
+
+            // b^2
+            T b2 = b.Multiply(b);
+            Console.WriteLine("b^2 = " + b2);
+
+            // a^2 - b^2
+            T diffSquares = a2.Subtract(b2);
+            Console.WriteLine("a^2 - b^2 = " + diffSquares);
+
+            // a + b
+            T sum = a.Add(b);
+            Console.WriteLine("a + b = " + sum);
+
+            try
+            {
+                T result = diffSquares.Divide(sum);
+                Console.WriteLine("(a^2-b^2)/(a+b) = " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Result: Division by Zero (a+b equals 0)");
+            }
+
+            Console.WriteLine("=== Finishing testing (a-b) = (a^2-b^2)/(a+b) ===");
+            Console.WriteLine();
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860d0ff30fdd96df8f6b6eb785a460b9753efbe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7159f177f98425dc2c6a29b005606ec8f84207df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
